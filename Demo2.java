b9b2c6e82b7b0953444cc2c4aa82cfd9343d6696
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2023-09-22 13:08:23 +0530

    complete task

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+class Demo2{
+    public static void main(String[] args) {
+        int x = 5;
+        do{
+            
+        for(int i =0;i<5;i++){
+            System.out.print("*");
+        }
+        x--;
+        System.out.println();
+
+        }while(x!=0);
+        
+    }
+}
